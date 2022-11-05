3bdb4f686c90b31448315f0234188679b7aad799
chrisxcode <117528256+chrisxcode@users.noreply.github.com>
2022-11-05 18:16:51 +0200

    Create accordion.js

---
+++ JS Advanced/DOM Introduction - Exercise/accordion.js
@@ -0,0 +1,11 @@
+function toggle() {
+    let button = document.querySelector(`span.button`);
+    let extra = document.querySelector(`#extra`);
+    if (button.textContent == `More`) {
+        button.textContent = `Less`;
+        extra.style.display = `block`;
+    } else {
+        button.textContent = `More`;
+        extra.style.display = `none`;
+    }
+}

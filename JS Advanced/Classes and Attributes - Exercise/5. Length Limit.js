bb654acab9ef90e2eb27e4f203af31cd8f31bbd3
chrisxcode <117528256+chrisxcode@users.noreply.github.com>
2022-11-05 19:01:00 +0200

    Create 5. Length Limit.js

---
+++ JS Advanced/Classes and Attributes - Exercise/5. Length Limit.js
@@ -0,0 +1,32 @@
+class Stringer {
+    constructor(string, length) {
+        this.innerString = string;
+        this.innerLength = length;
+    }
+
+    increase(value) {
+        if (this.innerLength + value < 0) {
+            this.innerLength = 0;
+        } else {
+            this.innerLength += value;
+        }
+        
+    }
+
+    decrease(value) {
+        if (this.innerLength - value < 0) {
+            this.innerLength = 0;
+        } else {
+            this.innerLength -= value;
+        }
+    }
+
+    toString() {
+        if (this.innerLength < this.innerString.length) {
+            return `${this.innerString.slice(0, this.innerLength)}...`;
+        } else {
+            return this.innerString;
+        }
+    }
+
+}

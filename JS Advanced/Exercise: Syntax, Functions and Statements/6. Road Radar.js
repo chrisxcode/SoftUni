b7cd91a683e589c64a34f8dce901dae5055cc6d1
chrisxcode <117528256+chrisxcode@users.noreply.github.com>
2022-11-05 17:28:48 +0200

    Create 6. Road Radar.js

---
+++ JS Advanced/Exercise: Syntax, Functions and Statements/6. Road Radar.js
@@ -0,0 +1,39 @@
+function roadRadar(speed, area) {
+    
+    let limit = 0;
+
+    switch (area) {
+        case `motorway`:
+            limit = 130;
+            break;
+        case `interstate`:
+            limit = 90;
+            break;
+        case `city`:
+            limit = 50;
+            break;
+        case `residential`:
+            limit = 20;
+            break;
+        default:
+            break;
+    }
+
+    if (speed <= limit) {
+        console.log(`Driving ${speed} km/h in a ${limit} zone`);
+    } else {
+        let difference = speed - limit;
+        let severity;
+
+            if (difference <= 20) {
+                severity = `speeding`;
+            } else if (difference <= 40) {
+                severity = `excessive speeding`;
+            } else if (difference > 40) {
+                severity = `reckless driving`;
+            }
+
+            console.log(`The speed is ${difference} km/h faster than the allowed speed of ${limit} - ${severity}`);
+    }
+
+}
